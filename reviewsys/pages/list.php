230bc9f1c4e2d28a96c0b7ec76217c1e20d24ca1
C180005 <c180005@ccg.ac.jp>
2020-01-23 18:58:37 +0900

    課題１提出

--- reviewsys/pages/list.php
+++ reviewsys/pages/list.php
@@ -1,10 +1,10 @@
 <?php require 'header.tpl';?>
-	<main>
-		<div class="clearfix">
-			<h2>レストラン一覧</h2>
-			<form action="list_selected.php" name="search_form" method="get">
+<main>
+	<div class="clearfix">
+		<h2>地域を選んで下さい</h2>
+		<form action="list_selected.php" name="search_form" method="get">
 <?php
-require 'list_common.php';
+require 'list_all.php';
 $area = [
     '-- 地域を選んで下さい --',
     '福岡',
@@ -18,29 +18,10 @@ for ($i = 0; $i < count($area); $i ++) {
 $listbox .= "</select>\n";
 echo "{$listbox}";
 ?>
-					<input type="submit" value="検索" />
-			</form>
-		</div>
+			<input type="submit" value="検索" />
+		</form>
+	</div>
 		
-		<table class="list">
-<?php
-foreach ($shop_no as $tt) {
-    if ($tt != "shop0") {
-        echo "<tr>
-              <td class='photo'><img width='110' alt={${$tt}['photo_alt']}
-				    src={${$tt}['photo_src']}/>
-              </td> 
-				<td class='info'>
-				    <dl>
-                    <dt>{${$tt}['photo_title']}</dt>
-                    <dd>{${$tt}['photo_desc']} </dd>
-                  </dl>
-               </td>
-               <td class='detail'><a href='detail.php?id={${$tt}['id']}'>詳細</a></td>
-               </tr>";
-    }
-}
-?>
-    </table >
-	</main>
-	<?php require 'footer.tpl';?>
+<?php //require 'shop_table.php'; ?>
+</main>
+<?php require 'footer.tpl';?>
--- reviewsys/pages/list_selected.php
+++ reviewsys/pages/list_selected.php
@@ -1,45 +1,22 @@
 <?php require 'header.tpl';?>
-	<main>
-		<div class="clearfix">
+<main>
+	<div class="clearfix">
 <?php
-require 'list_common.php';
+require 'list_all.php';
 $area_selected = @$_GET['area'];
 if ($area_selected == '-- 地域を選んで下さい --')
-    echo '<h2>地域を選択してください</h2>';
+    header('Location: list.php');
 else
     echo '<h2>' . $area_selected . 'のレストラン</h2>';
-?>
-
-<a href="list.php"><button type="button" class="btn btn-success">レストラン一覧に戻る</button></a>
-
-<?php
+echo '<a href="list.php"><button type="button" class="btn btn-success">他の地域を選択</button></a>';
 $shop_no_selected[] = shop0;
 foreach ($shop_no as $val) {
     if ($$val['area'] == $area_selected) {
         $shop_no_selected[] = $val;
     }
 }
-
-?>
-			<table class="list">
-<?php
-foreach ($shop_no_selected as $tt) {
-    if ($tt != "shop0") {
-        echo "<tr>
-              <td class='photo'><img width='110' alt={${$tt}['photo_alt']}
-				    src={${$tt}['photo_src']}/>
-              </td> 
-				<td class='info'>
-				    <dl>
-                    <dt>{${$tt}['photo_title']}</dt>
-                    <dd>{${$tt}['photo_desc']} </dd>
-                  </dl>
-               </td>
-               <td class='detail'><a href='detail.php?id={${$tt}['id']}'>詳細</a></td>
-               </tr>";
-    }
-}
-?>
-    </table></main>
+$shop_no = $shop_no_selected; 
+require 'shop_table.php'; ?>
+</main>
 <?php require 'footer.tpl';?>
     
